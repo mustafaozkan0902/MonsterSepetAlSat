38d5bdf189ffe3c1835227c3f4ae14f6919cb360
mustafa ozkan <43502579+mustafaozkan0902@users.noreply.github.com>
2025-09-20 01:07:39 +0300

    Create keyler.py

---
+++ keyler.py
@@ -0,0 +1,6 @@
+api_key = 'MtcdQfzliFDkAGnWrVDAoHuWo2oefWFRsBZuv9FRdBfbALCJZGGCWsLvwU5n8xRG'
+api_secret = 'h8vqo5JzKpUfHzglNThJGxgLZ6IQ1wvC3E4q9rwkjoQFI74I7DRpjBx6O470aog7'
+
+
+telegram_token = "8344861814:AAHPDWvZSUGRFkhPuvT-6WYizWGs3t4RTCo"
+telegram_chat_id = "964255918"
